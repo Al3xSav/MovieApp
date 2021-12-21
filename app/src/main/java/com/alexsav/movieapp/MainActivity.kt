f18daf227699df086529e293100c1d3116e0be93
Al3xSav <alex.savtsouk@gmail.com>
2021-12-22 01:35:49 +0200

    Second Project for Android Kotlin JetPack <<MovieApp>>

---
+++ app/src/main/java/com/alexsav/movieapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.alexsav.movieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.alexsav.movieapp.navigation.MovieNavigation
+import com.alexsav.movieapp.ui.theme.MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MovieNavigation()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        content()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MovieNavigation()
+    }
+}
---
+++ app/src/main/java/com/alexsav/movieapp/screens/HomeScreen.kt
@@ -0,0 +1,52 @@
+package com.alexsav.movieapp.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.alexsav.movieapp.model.Movie
+import com.alexsav.movieapp.model.getMovies
+import com.alexsav.movieapp.navigation.MovieScreens
+import com.alexsav.movieapp.widgets.MovieRow
+
+@Composable
+fun HomeScreen(navController: NavController) {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = Color.Transparent,
+                elevation = 0.dp
+            ) {
+                Text(text = "Movies")
+            }
+        },
+    ) {
+        MainContent(navController)
+    }
+}
+
+@Composable
+fun MainContent(
+    navController: NavController,
+    movieList: List<Movie> = getMovies()
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it) { movie ->
+
+                    navController.navigate(route = MovieScreens.DetailsScreen.name + "/$movie")
+                }
+            }
+        }
+    }
+}
